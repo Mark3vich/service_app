dc79dc8fdd14e664b4234577319d0fa261f81541
Mark3vich <mango.2004@yandex.ru>
2024-02-18 16:19:12 +0300

    OPTIMIZED QUERIES

--- service/services/views.py
+++ service/services/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from services.serializers import SubscriptionSerializer
 from services.models import Subscription
+from django.db.models import Prefetch
+from clients.models import Client
 
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # prefetch_related сэкономит запросы к бд и соответсаенно оптимизирует работу программы. Проблема n + 1 - решена 
+    queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+        )
     serializer_class = SubscriptionSerializer
