93a14347f296cc8a7c0a06eb6f507637484bc1a0
Mark3vich <mango.2004@yandex.ru>
2024-02-20 18:58:02 +0300

    20.02.2024

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -14,8 +14,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     price = serializers.SerializerMethodField() # связан с get_price т.к ищет поле price
 
     def get_price(self, instance): 
-        return (instance.service.full_price - 
-                instance.service.full_price * (instance.plan.discount_percent / 100))
+        return instance.price
 
     class Meta: 
         model = Subscription
--- service/services/views.py
+++ service/services/views.py
@@ -4,6 +4,7 @@ from services.serializers import SubscriptionSerializer
 from services.models import Subscription
 from django.db.models import Prefetch
 from clients.models import Client
+from django.db.models import F
 
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
@@ -11,5 +12,5 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-        )
+        ).annotate(price=F('service__full_price')-F('service__full_price')*F('plan__discount_percent')/100.00)
     serializer_class = SubscriptionSerializer
